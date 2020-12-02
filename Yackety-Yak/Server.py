2d3d5b9604a6ed90d36fcdcad069979b7a9fe3c3
x-0117 <72228744+x-0117@users.noreply.github.com>
2020-12-03 01:00:54 +0530

    Update Server.py

--- Yackety-Yak/Server.py
+++ Yackety-Yak/Server.py
@@ -1,52 +1,37 @@
-import socket, _thread, base64, random
+import threading, socket, random
+_receive_ = []
+clients = []
 s = socket.socket()
 s.bind(('', 12345))
-s.listen(5)
-print("Listening!")
-c, addr = s.accept()
-print("Connected")
-x = random.randint(0, 256)
-c.send(str.encode(str(x)))
+print("Bound")
+s.listen()
+message_count = 0
+message = ''
+count = 0
+key = random.randint(0, 256)
 
-
-def send():
+def receive():
+    global t1
+    global message
+    global message_count
+    print("receive started!")
+    c, addr = s.accept()
+    c.send(str.encode(str(key + 10)))
+    clients.append(c)
+    _receive_.append(t1)
+    print("Received connection!")
+    t1 = threading.Thread(target=receive)
+    t1.start()
     while True:
-        message = input("\t\t\t\t")
-        if message[:6] == "!file:":
-            message = "!file:" + str(base64.b64encode(open(message[6:], 'rb').read()))
-        print('_', message)
-        a, l, flag = 0, len(message), 0
-        while flag == 0:
-            if l - a <= 1000:
-                section = message[a:l] + '!end'
-                flag = 1
-            else:
-                section = message[a:a + 1000]
-            if a != 0:
-                section = "!cont:" + section
-            a += 1000
-            l1 = list(message)
-            for i in range(len(message)):
-                l1[i] = chr(ord(l1[i]) ^ x)
-            c.send(str.encode(''.join(l1)))
-    
+        message = str(c.recv(1024), "utf-8")
+        message_count += 1
+        for i in range(len(clients)):
+            if clients[i] != c:
+                clients[i].send(str.encode(str(addr[1])  + ' _:_ ' + message))
 
-_thread.start_new_thread(send, ())
-num = 0
+t1 = threading.Thread(target=receive)
+
+t1.start()
 while True:
-    l1 = list(str(c.recv(2048), "utf-8"))
-    for i in range(len(l1)):
-        l1[i] = chr(ord(l1[i]) ^ x)
-    reply = ''.join(l1)
-    base = ''
-    if reply[:6] == "!file:":
-        num += 1
-        base += base64.b64decode(reply[6:])
-        if reply[-4:] == "!end":
-            open("C:/Users/User/Desktop/Server/shit{}".format(num), 'ab').write(base64.b64decode(shit)
-    # elif reply[:6] == "!cont:":
-    #     base += base64.b64decode(reply[6:])
-    #     if reply[-4:] == "!end":
-    #         open("C:/Users/User/Desktop/Server/shit{}".format(num), 'ab').write(base64.b64decode(shit)
-    # else:
-    #     print(reply)
+    if count != message_count:
+        count += 1
