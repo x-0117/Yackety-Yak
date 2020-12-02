8909b5a97c8f78b68a67e322bfba819ec9193655
x-0117 <72228744+x-0117@users.noreply.github.com>
2020-12-03 00:59:45 +0530

    Update Client.py

--- Yackety-Yak/Client.py
+++ Yackety-Yak/Client.py
@@ -1,49 +1,18 @@
-import socket, _thread, base64
+import socket, threading
 s = socket.socket()
 s.connect((socket.gethostbyname('localhost'), 12345))
 print("Connected!")
-x = int(str(s.recv(2048), "utf-8"))
-print("x = ", x)
-
-
+public_key = int(str(s.recv(1024), "utf-8")) - 10
 def send():
     while True:
-        message = input("\t\t\t\t")
-        if message[:6] == "!file:":
-            message = "!file:" + str(base64.b64encode(open(message[6:], 'rb').read()))
-        print('_', message)
-        a, l, flag = 0, len(message), 0
-        while flag == 0:
-            if l - a <= 1000:
-                section = message[a:l] + '!end'
-                flag = 1
-            else:
-                section = message[a:a + 1000]
-            if a != 0:
-                section = "!cont:" + section
-            a += 1000
-            l1 = list(message)
-            for i in range(len(message)):
-                l1[i] = chr(ord(l1[i]) ^ x)
-            s.send(str.encode(''.join(l1)))
-    
-
-_thread.start_new_thread(send, ())
-num = 0
+        message = list(input('>>'))
+        for i in range(len(message)):
+            message[i] = chr(ord(message[i]) ^ public_key)
+        s.send(str.encode(''.join(message)))
+threading.Thread(target=send).start()
 while True:
-    l1 = list(str(s.recv(2048), "utf-8"))
-    for i in range(len(l1)):
-        l1[i] = chr(ord(l1[i]) ^ x)
-    reply = ''.join(l1)
-    base = ''
-    if reply[:6] == "!file:":
-        num += 1
-        base += base64.b64decode(reply[6:])
-        if reply[-4:] == "!end":
-            open("C:/Users/User/Desktop/Server/shit{}".format(num), 'ab').write(base64.b64decode(shit)
-    elif reply[:6] == "!cont:":
-        base += base64.b64decode(reply[6:])
-        if reply[-4:] == "!end":
-            open("C:/Users/User/Desktop/Server/shit{}".format(num), 'ab').write(base64.b64decode(shit)
-    else:
-        print(reply)
+    port, message1 = str(s.recv(1024), "utf-8").split(' _:_ ')
+    message2 = list(message1)
+    for i in range(len(message2)):
+        message2[i] = chr(ord(message2[i]) ^ public_key)
+    print(port, ':', ''.join(message2))
